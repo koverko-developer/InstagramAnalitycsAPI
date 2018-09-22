dd78d06929d096cad1d1e1cc12e48b2de32986da
koverko-developer <kowerkodeveloper@gmail.com>
2018-09-22 13:53:38 +0300

    Add files via upload

--- top/TopController.js
+++ top/TopController.js
@@ -20,16 +20,18 @@ top.use(bodyParser.urlencoded({ extended: true }));
 top.use(bodyParser.json());
 
 
-top.route('/info/')
+top.route('/:id/comments')
     .get((req, res) => {
+
     })
     .post((req, res) => {
+      chekoutInF(req, res, 1);
     })
 top.route('/:id/likes')
     .get((req, res) => {
     })
     .post((req, res) => {
-      chekoutInF(req, res);
+      chekoutInF(req, res, 2);
     })
 
 function getUserInfo(req, res) {
@@ -53,7 +55,7 @@ function getUserInfo(req, res) {
 
 }
 
-function chekoutInF(req, res){
+function chekoutInF(req, res, type){
     let userId = req.params.id;
     console.log(userId);
     //console.log(error);
@@ -65,13 +67,11 @@ function chekoutInF(req, res){
       if(access_token === 'Anonymous'){
           res.send(error);
       }else{
-          getCookie(req, res, username, userId, username);
+          getCookie(req, res, username, userId, username, type);
       }
     });
 }
-function getCookie(req,res, username, accountId, user) {
-
-
+function getCookie(req,res, username, accountId, user, type) {
 
   var dir = __dirname + "/cookies/"+username+".json";
   dir = dir.replace('top', 'auth');
@@ -87,7 +87,8 @@ function getCookie(req,res, username, accountId, user) {
     .on('data', function (chunk) {
       d = chunk;
       //console.log(d);
-      setCookie(d, res, count, session, accountId, dir, user);
+      if(type === 1) setCookie(d, res, count, session, accountId, dir, user);
+      else if(type === 2) setCookieLikes(d, res, count, session, accountId, dir, user);
     })
     .on('end', function () {
         console.log('All the data in the file has been read');
@@ -124,15 +125,7 @@ function setCookie(data, res, count_m, session, accountId, dir, user) {
             const feed = new Client.Feed.MediaComments(session, media[k]['id']);
         		let originalCursor = feed.getCursor();
         		feed.get().then(function(comments) {
-
-        			  //console.log(comments);
               _.each(comments, function(comment) {
-                        // console.log('-------------------------------');
-                        // console.log(comment['params']['account']['username'] + " - " +comment['params']['account']['fullName']);
-                        // console.log(comment['params']['text']);
-                        // console.log(comment['params']['account']['picture']);
-
-
                         var uname = comment['params']['account']['username'];
                         var picture = comment['params']['account']['picture'];
                         if(uname != user){
@@ -173,6 +166,98 @@ function setCookie(data, res, count_m, session, accountId, dir, user) {
                 });
         }
       }
+
+    var end = new Date();
+    console.log('Цикл занял '+(end - start)+' ms');
+
+    var urls = _.map(media, function(medium) {
+       return _.last(medium)
+    });
+    //console.log(results);
+      fs.writeFile(dir, data , function(err) {
+        if(err) {
+        }else {
+
+        }
+      });
+    })
+
+}
+
+function setCookieLikes(data, res, count_m, session, accountId, dir, user) {
+  var topUsers = []
+  var usersAll = []
+  var usersSort = []
+
+  var start = new Date();
+  var feed = new Client.Feed.UserMedia(session, accountId);
+  console.log('count media = ' + count_m);
+      Promise.mapSeries(_.range(count_m), function() {
+       return feed.get();
+      })
+      .then(function(results) {
+      // result should be Media[][]
+      var media = _.flatten(results);
+      //console.log(media[1]);
+      var count_promise = 0;
+      var count_promise_true = 0;
+      for(var k in media){
+          //console.log(media[k]['_params']['topLikers']);
+          var topL = media[k]['_params']['topLikers'];
+          for(var k in topL){
+            //console.log(topL[k]);
+            usersAll[usersAll.length] = topL[k];
+            if(usersSort.indexOf(topL[k]) === -1) {
+              usersSort[usersSort.length] = topL[k];
+            }
+          }
+
+      }
+
+      for(var k in usersSort){
+        var col = 0;
+
+        for(var j in usersAll){
+          if(usersAll[j] == usersSort[k]) col++;
+        }
+
+        var usertop = new TopUser(usersSort[k], '', '');
+        usertop.setcount_comments(col);
+        topUsers.push(usertop);
+        topUsers.sort(compare);
+        if(topUsers.length > 5)topUsers.length = 5;
+      }
+
+      var newUserTops = []
+
+      for(var k in topUsers){
+        count_promise++;
+        console.log(topUsers[k].getusername());
+        Client.Account.searchForUser(session, topUsers[k].getusername())
+        .then(function(account) {
+           var usernames = account['_params']['username'];
+           var fillnames = account['_params']['fullName'];
+           //console.log(account['_params']['profilePicUrl']);
+           var pic = account['_params']['profilePicUrl'];
+           //console.log('-------------------------');
+           var usertop = new TopUser(usernames, fillnames, pic);
+           newUserTops.push(usertop);
+           count_promise_true++;
+           if(count_promise_true == count_promise) {
+             res.send(newUserTops)
+           }else {
+             console.log('wait');
+           }
+           return account.id; })
+        .catch(function(err) {
+          console.error(err.message)}
+        );
+      }
+
+      //res.send(topUsers)
+
+      //console.log(usersAll);
+      //console.log(usersSort);
 
     var end = new Date();
     console.log('Цикл занял '+(end - start)+' ms');
