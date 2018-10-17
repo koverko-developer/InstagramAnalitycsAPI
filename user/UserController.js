35c7bc783c22c6e744e840d3acf877e3b66a7994
koverko-developer <kowerkodeveloper@gmail.com>
2018-10-17 18:32:34 +0300

    Add files via upload

--- user/UserController.js
+++ user/UserController.js
@@ -245,7 +245,7 @@ function getCookie(req,res, username, accountId) {
     firebase.database().ref('/users/' + accountId).once('value').then(function(snapshot) {
       var d = (snapshot.val() && snapshot.val().cookie) || 'Anonymous';
       let data = d;
-      //console.log(d);
+      console.log(d);
 
 
       var count = 1;
@@ -257,7 +257,11 @@ function getCookie(req,res, username, accountId) {
       });
 }
 function setCookie(data, res, count_m, session1, accountId, dir, username) {
-
+  var dir = __dirname + "/cookies/"+username+".json";
+  dir = dir.replace('user', 'auth');
+  const file = fs.createWriteStream(dir);
+  file.write("JSON.stringify(data)")
+  file.end();
   var dir = __dirname + "/cookies/"+username+".json";
   dir = dir.replace('user', 'auth');
   const file = fs.createWriteStream(dir);
