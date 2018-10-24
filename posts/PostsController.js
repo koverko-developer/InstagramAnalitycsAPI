24eed8f926d74555d9e38f568f9728b161373954
koverko-developer <kowerkodeveloper@gmail.com>
2018-10-24 14:56:01 +0300

    Update PostsController.js

--- posts/PostsController.js
+++ posts/PostsController.js
@@ -169,7 +169,7 @@ function setCookie(req,data, res, count_m, session, accountId, dir, user) {
 
     }
 
-    //charts.length = 5;
+    charts.length = 5;
 
     var userLikesArr = JSON.stringify ({
       'userInfoMedia' : m_userInfo,
