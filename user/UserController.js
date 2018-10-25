59334f9caa7ba441e1bb8bd64b0b13426c850116
koverko-developer <kowerkodeveloper@gmail.com>
2018-10-25 17:59:30 +0300

    Add files via upload

--- user/UserController.js
+++ user/UserController.js
@@ -498,7 +498,7 @@ function setCookieC(data, res, count_m, session, accountId, dir, username) {
                             firebase.database().ref('/users/' + accountId + "/top/comments/").set({
                                 value: topUsers,
                               });
-                              //setCookieLikes(data, res, count_m, session, accountId, dir, username)
+                              setCookieLikes(data, res, count_m, session, accountId, dir, username)
                           }else {
                             console.log('waiting '+ count_promise_true+' from '+count_promise);
                           }
