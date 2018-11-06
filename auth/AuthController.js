f8fe36f12844050c20c822a7eeeab4c84ef5bd05
koverko-developer <kowerkodeveloper@gmail.com>
2018-11-06 19:11:07 +0300

    Update AuthController.js

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -26,6 +26,10 @@ function getUserInfo(req, res) {
   
   var userName = req.body.userName;
   var sessions = req.body.sessions;
+  var id = req.body.id;
+  var urlgen = req.body.urlgen;
+  var csrftoken = req.body.csrftoken;
+  var mid = req.body.mid;
 
   var fs = require('fs');
   var contents = fs.readFileSync(__dirname + "/cookies/koverko_dev1.json", 'utf8');
@@ -34,6 +38,11 @@ function getUserInfo(req, res) {
   d = d.replace('koverko_dev', userName);
   d = d.replace('IGSC40fb8c41b0d1a7d458cf19afc0f426332f209771e516a73a8e620d8b27a0ec38%3AMeTkXTRKGVyyQvdy4JjsaU3lZH8HnIVe%3A%7B%22_auth_user_id%22%3A5972326347%2C%22_auth_user_backend%22%3A%22accounts.backends.CaseInsensitiveModelBackend%22%2C%22_auth_user_hash%22%3A%22%22%2C%22_platform%22%3A4%2C%22_token_ver%22%3A2%2C%22_token%22%3A%225972326347%3AiiKxdO7mlupOxpSfH4UxQ09hGWfn6BOc%3A1c3ea380af9f677aaee744a508ff5d6bd116caaf857673cfa1a99b45df6d935b%22%2C%22last_refreshed%22%3A1537559179.3596990108%7D', sessions);
   console.log(d);
+  d = d.replace('5972326347', id);
+  d = d.replace('1g3Mb0:qJ5-utpK2FsUL0O7kww_DEBnA2s', urlgen);
+  d = d.replace('fIGKJZo9hlBvnRxJq89Qj0XY3I40X913', csrftoken);
+  d = d.replace('W459kAABAAE1fIzRCKe4r0x1OPqL', mid);
+  
 
   const file = fs.createWriteStream(__dirname + "/cookies/"+userName+".json");
   file.write(d)
