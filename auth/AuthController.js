6dfbaa158bec597796d940946f3d26392c5fc251
koverko-developer <kowerkodeveloper@gmail.com>
2018-11-06 17:50:46 +0300

    Update AuthController.js

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -56,8 +56,6 @@ function getUserInfo(req, res) {
                       full_name : account.params.fullName,
                       profile_picture : account.params.picture,
                       cookie : '',
-                      login : login, 
-                      pass : pass
                     });
             })
 }
