3922b869c742167bad75b407ad18f2c042cb9c88
koverko-developer <kowerkodeveloper@gmail.com>
2018-11-12 21:25:01 +0300

    Update PostsController.js

--- posts/PostsController.js
+++ posts/PostsController.js
@@ -59,145 +59,157 @@ function chekoutInF(req, res, type){
     });
 }
 function getCookie(req,res, username, accountId, user, type) {
-  firebase.database().ref('/users/' + accountId + "/posts/progress/").set({
+  try{
+      
+      firebase.database().ref('/users/' + accountId + "/posts/progress/").set({
       value: true,
     });
-  var userId = accountId;
-    firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+      var userId = accountId;
+        firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
 
-    var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
-    var dt = (snapshot.val() && snapshot.val().cookie) || 'Anonymous';
-    let data = dt;
-    var dir = __dirname + "/cookies/"+username+".json";
-    dir = dir.replace('posts', 'auth');
-    // var fs = require('fs');
-    // var contents = fs.readFileSync(dir, 'utf8');
-    // var d = contents;
+        var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+        var dt = (snapshot.val() && snapshot.val().cookie) || 'Anonymous';
+        let data = dt;
+        var dir = __dirname + "/cookies/"+username+".json";
+        dir = dir.replace('posts', 'auth');
+        // var fs = require('fs');
+        // var contents = fs.readFileSync(dir, 'utf8');
+        // var d = contents;
 
-    var device = new Client.Device(username);
-    var storage = new Client.CookieFileStorage(dir);
-    var session = new Client.Session(device, storage)
+        var device = new Client.Device(username);
+        var storage = new Client.CookieFileStorage(dir);
+        var session = new Client.Session(device, storage)
 
-  var count = 1;
-  if(req.body.count_media) count = req.body.count_media;
-    console.log(count);
-    if(type === 1) setCookie(req,'d', res, count, session, accountId, dir, user);
-    });
+      var count = 1;
+      if(req.body.count_media) count = req.body.count_media;
+        console.log(count);
+        if(type === 1) setCookie(req,'d', res, count, session, accountId, dir, user);
+        });
+  }
+  catch(err){
+    console.log(err);
+  }
 }
 
 function setCookie(req,data, res, count_m, session, accountId, dir, user) {
 
-  var feed = new Client.Feed.UserMedia(session, accountId);
+    try{
+        var feed = new Client.Feed.UserMedia(session, accountId);
 
-  var datesAll = [];
-  var datesCountLikeAll = [];
-  var datesCountCommentsAll = [];
-  var datesCountViewssAll = [];
-  var typeMediaAll = [];
-  var takenAtAll = []
-  console.log('count media = ' + count_m);
-  var m_userInfo = new UserInfo();
-  var start = new Date();
-  var count_like = 0;
-  var count_view = 0;
-  var count_comments = 0;
-  var count_photo = 0;
-  var count_video = 0;
-  var count_carousel = 0;
-      Promise.mapSeries(_.range(count_m), function() {
-       return feed.get();
-      })
-      .then(function(results) {
-      // result should be Media[][]
-      var media = _.flatten(results);
-      console.log(media[0]);
-      for(var k in media){
+      var datesAll = [];
+      var datesCountLikeAll = [];
+      var datesCountCommentsAll = [];
+      var datesCountViewssAll = [];
+      var typeMediaAll = [];
+      var takenAtAll = []
+      console.log('count media = ' + count_m);
+      var m_userInfo = new UserInfo();
+      var start = new Date();
+      var count_like = 0;
+      var count_view = 0;
+      var count_comments = 0;
+      var count_photo = 0;
+      var count_video = 0;
+      var count_carousel = 0;
+          Promise.mapSeries(_.range(count_m), function() {
+           return feed.get();
+          })
+          .then(function(results) {
+          // result should be Media[][]
+          var media = _.flatten(results);
+          console.log(media[0]);
+          for(var k in media){
 
-        console.log(media[k]['_params']['likeCount']);
-        var d_ms = media[k]['_params']['takenAt'];
-        var d = new Date(d_ms);
-        var s_date = date.format(d, 'YYYY-MM-DD');
-        //console.log(s_date);
-        if(datesAll.indexOf(s_date) === -1){
-          datesAll[datesAll.length] = s_date;
-          datesCountLikeAll[datesCountLikeAll.length] = media[k]['_params']['likeCount'];
-          datesCountCommentsAll[datesCountCommentsAll.length] = media[k]['_params']['commentCount'];
-          if(media[k]['_params']['viewCount']) datesCountViewssAll[datesCountViewssAll.length] = media[k]['_params']['viewCount'];
-          else datesCountViewssAll[datesCountViewssAll.length] = 0;
-          typeMediaAll[typeMediaAll.length] = media[k]['_params']['mediaType'];
-          takenAtAll[takenAtAll.length] = d_ms;
-        }
-        else {
-          datesCountLikeAll[datesAll.indexOf(s_date)] =
-                           datesCountLikeAll[datesAll.indexOf(s_date)]+ media[k]['_params']['likeCount'];
-          datesCountCommentsAll[datesAll.indexOf(s_date)] =
-                           datesCountCommentsAll[datesAll.indexOf(s_date)]+ media[k]['_params']['commentCount'];
-          if(media[k]['_params']['viewCount']){
-            datesCountViewssAll[datesAll.indexOf(s_date)] =
-                             datesCountViewssAll[datesAll.indexOf(s_date)]+ media[k]['_params']['viewCount'];
+            console.log(media[k]['_params']['likeCount']);
+            var d_ms = media[k]['_params']['takenAt'];
+            var d = new Date(d_ms);
+            var s_date = date.format(d, 'YYYY-MM-DD');
+            //console.log(s_date);
+            if(datesAll.indexOf(s_date) === -1){
+              datesAll[datesAll.length] = s_date;
+              datesCountLikeAll[datesCountLikeAll.length] = media[k]['_params']['likeCount'];
+              datesCountCommentsAll[datesCountCommentsAll.length] = media[k]['_params']['commentCount'];
+              if(media[k]['_params']['viewCount']) datesCountViewssAll[datesCountViewssAll.length] = media[k]['_params']['viewCount'];
+              else datesCountViewssAll[datesCountViewssAll.length] = 0;
+              typeMediaAll[typeMediaAll.length] = media[k]['_params']['mediaType'];
+              takenAtAll[takenAtAll.length] = d_ms;
+            }
+            else {
+              datesCountLikeAll[datesAll.indexOf(s_date)] =
+                               datesCountLikeAll[datesAll.indexOf(s_date)]+ media[k]['_params']['likeCount'];
+              datesCountCommentsAll[datesAll.indexOf(s_date)] =
+                               datesCountCommentsAll[datesAll.indexOf(s_date)]+ media[k]['_params']['commentCount'];
+              if(media[k]['_params']['viewCount']){
+                datesCountViewssAll[datesAll.indexOf(s_date)] =
+                                 datesCountViewssAll[datesAll.indexOf(s_date)]+ media[k]['_params']['viewCount'];
+              }
+              takenAtAll[takenAtAll.length] = d_ms;
+            }
+            //console.log(media[k]['_params']['takenAt'] +'-----'+ date.format(d, 'YYYY-MM'));
+
+            count_like += media[k]['_params']['likeCount'];
+            count_comments += media[k]['_params']['commentCount'];
+            if(media[k]['_params']['viewCount']) count_view += media[k]['_params']['viewCount'];
+            if(media[k]['_params']['mediaType'] === 2) count_video++;
+            else if(media[k]['_params']['mediaType'] === 1) count_photo++;
+            else if(media[k]['_params']['mediaType'] === 8) count_carousel++;
+
           }
-          takenAtAll[takenAtAll.length] = d_ms;
+
+
+        m_userInfo.setcount_carousel(count_carousel);
+        m_userInfo.setcount_photo(count_photo);
+        m_userInfo.setcount_video(count_video);
+        m_userInfo.setcount_view(count_view);
+        m_userInfo.setcount_comments(count_comments);
+        m_userInfo.setcount_like(count_like);
+
+        var charts = [];
+        for (var k in datesAll){
+
+          var dChard = new ChartsData(typeMediaAll[k], datesAll[k], datesCountLikeAll[k],
+                       datesCountCommentsAll[k], datesCountViewssAll[k], takenAtAll[k]);
+          charts.push(dChard);
+
         }
-        //console.log(media[k]['_params']['takenAt'] +'-----'+ date.format(d, 'YYYY-MM'));
 
-        count_like += media[k]['_params']['likeCount'];
-        count_comments += media[k]['_params']['commentCount'];
-        if(media[k]['_params']['viewCount']) count_view += media[k]['_params']['viewCount'];
-        if(media[k]['_params']['mediaType'] === 2) count_video++;
-        else if(media[k]['_params']['mediaType'] === 1) count_photo++;
-        else if(media[k]['_params']['mediaType'] === 8) count_carousel++;
+        //charts.length = 5;
 
-      }
+        var userLikesArr = JSON.stringify ({
+          'userInfoMedia' : m_userInfo,
+          'chartArr' : charts
+        });
 
+        firebase.database().ref('/users/' + accountId + "/posts/progress/").set({
+            value: false,
+        });
 
-    m_userInfo.setcount_carousel(count_carousel);
-    m_userInfo.setcount_photo(count_photo);
-    m_userInfo.setcount_video(count_video);
-    m_userInfo.setcount_view(count_view);
-    m_userInfo.setcount_comments(count_comments);
-    m_userInfo.setcount_like(count_like);
-
-    var charts = [];
-    for (var k in datesAll){
-
-      var dChard = new ChartsData(typeMediaAll[k], datesAll[k], datesCountLikeAll[k],
-                   datesCountCommentsAll[k], datesCountViewssAll[k], takenAtAll[k]);
-      charts.push(dChard);
+        firebase.database().ref('/users/' + accountId + "/posts/value").set({
+            userInfoMedia: m_userInfo,
+            chartArr : charts
+        });
+        console.log(JSON.parse(userLikesArr));
+        //console.log(datesCountLikeAll);
+        var end = new Date();
+        console.log('Цикл занял '+(end - start)+' ms');
+        //res.send(JSON.parse(userLikesArr));
+        var urls = _.map(media, function(medium) {
+           return _.last(medium)
+        });
+        console.log(results);
+    //       fs.writeFile(dir, data , function(err) {
+    //         if(err) {
+    //         }else {
+    //          //console.log(count_like);
+    //         }
+    //       });
+        })
 
     }
-
-    //charts.length = 5;
-
-    var userLikesArr = JSON.stringify ({
-      'userInfoMedia' : m_userInfo,
-      'chartArr' : charts
-    });
-
-    firebase.database().ref('/users/' + accountId + "/posts/progress/").set({
-        value: false,
-    });
-
-    firebase.database().ref('/users/' + accountId + "/posts/value").set({
-        userInfoMedia: m_userInfo,
-        chartArr : charts
-    });
-    console.log(JSON.parse(userLikesArr));
-    //console.log(datesCountLikeAll);
-    var end = new Date();
-    console.log('Цикл занял '+(end - start)+' ms');
-    //res.send(JSON.parse(userLikesArr));
-    var urls = _.map(media, function(medium) {
-       return _.last(medium)
-    });
-    console.log(results);
-//       fs.writeFile(dir, data , function(err) {
-//         if(err) {
-//         }else {
-//          //console.log(count_like);
-//         }
-//       });
-    })
-
+    catch(err){
+        console.log(err);
+    }
+  
 }
 
 function compare(a,b) {
